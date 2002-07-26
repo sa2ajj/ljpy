ee0fd667001c2260850c07a8b0e0caee6ae5826a
Mikhail Sobolev <mss@mawhrin.net>
2002-07-26 20:52:44 +0000

    setup script for ljpy

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+
+from distutils.core import setup
+
+setup (
+	name = "ljpy",
+	version = "0.0",
+	description = "Module and scripts for working with LiveJournal -- http://www.livejournal.com",
+	author = "Mikhail Sobolev",
+	author_email = "mss@mawhrin.net",
+	url = "http://only.mawhrin.net/~mss/lj/",
+	py_modules = ['livejournal'],
+)
