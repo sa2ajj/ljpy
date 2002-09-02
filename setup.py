a81a0ae898c3fbf470f5301899b731aa3a042c50
Mikhail Sobolev <mss@mawhrin.net>
2002-09-02 14:01:54 +0000

    adde.py is now known as ljp.py

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup (
 	author_email = "mss@mawhrin.net",
 	url = "http://only.mawhrin.net/~mss/lj/",
 	py_modules = ['livejournal'],
-    scripts = [ 'adde.py', 'ljac.py' ]
+    scripts = [ 'ljp.py', 'ljac.py' ]
 )
