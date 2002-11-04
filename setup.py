d9c0b3e43e392a55fea92df67631e4a0c0742438
Mikhail Sobolev <mss@mawhrin.net>
2002-11-04 17:59:15 +0000

    * changed the version (now I consider it to be 0.5)  * fixed the URL

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from distutils.core import setup
 
 setup (
 	name = "ljpy",
-	version = "0.1",
+	version = "0.5",
 	description = "Module and scripts for working with LiveJournal -- http://www.livejournal.com",
 	author = "Mikhail Sobolev",
 	author_email = "mss@mawhrin.net",
-	url = "http://only.mawhrin.net/~mss/lj/",
+	url = "http://only.mawhrin.net/~mss/python/lj/",
 	py_modules = ['livejournal'],
-    scripts = [ 'ljp.py', 'ljac.py' ]
+    scripts = [ 'ljp.py', 'ljac.py' ],
+    license = 'GPL v2'
 )
