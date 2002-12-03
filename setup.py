388e7621f6ce08a4c222a4393ded187fbd765737
Mikhail Sobolev <mss@mawhrin.net>
2002-12-03 18:56:02 +0000

    from now on livejournal is a package

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 
 setup (
 	name = "ljpy",
-	version = "0.5",
+	version = "0.6",
 	description = "Module and scripts for working with LiveJournal -- http://www.livejournal.com",
 	author = "Mikhail Sobolev",
 	author_email = "mss@mawhrin.net",
 	url = "http://only.mawhrin.net/~mss/python/lj/",
-	py_modules = ['livejournal'],
+	packages = ['livejournal'],
     scripts = [ 'ljp.py', 'ljac.py' ],
     license = 'GPL v2'
 )
