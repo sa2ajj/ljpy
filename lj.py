04ea82d2ceb80e0e86510862183e2ff508f2b9d7
Mikhail Sobolev <mss@mawhrin.net>
2003-10-19 10:05:53 +0000

    added a comment about the code: this is a sandbox

--- lj.py
+++ lj.py
@@ -1,20 +1,32 @@
 #! /usr/bin/python
 
-from os.path import expanduser
+'''this file is a sandbox for my experiments with differrent methods
+of the LiveJournal protocol.  Though I include this file in the distribution
+it should rather be used as an example...  Misha'''
 
-from livejournal import LiveJournal
-from config import Config
+from livejournal import LiveJournal, Config
 
 config = Config ()
-config.load ('lj.conf')
+config.load ('~/.ljrc')
 
-lj = LiveJournal ('Python-ljpy/0.0.1')
+server = config.server
+
+lj = LiveJournal (config.misc.version)
 
 from pprint import pprint
 
-pprint (lj.login (config.username, config.password))
+pprint (lj.login (server.username, server.password))
 
 if 0:
     pprint (lj.checkfriends (lastupdate = '2002-07-24 00:00:00', mask = 1))
-else:
+elif 0:
     pprint (lj.getfriends (includefriendof = 1, includegroups = 1))
+else:
+    print 'Synchronizing...'
+
+    value = lj.syncitems ()
+
+    print 'Got information for %d out of %d item(s)' % (value.count, value.total)
+
+    for item in value.syncitems:
+        print '%s (%s, %s) -- %s (%s)' % (item.item, item.itemid, item.type, item.time, item.action)
