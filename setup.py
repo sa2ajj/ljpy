eb78c0ddcfc0e9125a8c7b95d0b0d3d4f5972165
Mikhail Sobolev <mss@mawhrin.net>
2003-10-19 10:01:03 +0000

    * moving to the next version (0.8)  * made a better (I hope) short description  * added a long description  * fiexed the url to point to the right place

--- setup.py
+++ setup.py
@@ -4,12 +4,17 @@ from distutils.core import setup
 
 setup (
 	name = "ljpy",
-	version = "0.7",
-	description = "Module and scripts for working with LiveJournal -- http://www.livejournal.com",
+	version = "0.8",
+	description = "Module and scripts for working with LiveJournal servers",
+        long_description = '''\
+This is a simple implementation of XML-RPC variant of LiveJournal client-server
+protocol as of 
+    http://www.livejournal.com/doc/server/ljp.csp.xml-rpc.protocol.html
+''',
 	author = "Mikhail Sobolev",
 	author_email = "mss@mawhrin.net",
-	url = "http://only.mawhrin.net/~mss/python/lj/",
+	url = "http://only.mawhrin.net/~mss/thingies/ljpy/",
 	packages = ['livejournal'],
-    scripts = [ 'ljp.py', 'ljac.py' ],
-    license = 'GPL v2'
+        scripts = [ 'ljp.py', 'ljac.py' ],
+        license = 'GPL v2'
 )
