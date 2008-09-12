2dfda478178514b7c1196dcbe77bd76e03d6f973
Mikhail Sobolev <mss@mawhrin.net>
2008-09-12 18:28:54 +0000

    fixed the indenting for setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@
 from distutils.core import setup
 
 setup (
-	name = "ljpy",
-	version = "0.8",
-	description = "Module and scripts for working with LiveJournal servers",
-        long_description = '''\
+    name = "ljpy",
+    version = "0.8",
+    description = "Module and scripts for working with LiveJournal servers",
+    long_description = '''\
 This is a simple implementation of XML-RPC variant of LiveJournal client-server
 protocol as of 
     http://www.livejournal.com/doc/server/ljp.csp.xml-rpc.protocol.html
 ''',
-	author = "Mikhail Sobolev",
-	author_email = "mss@mawhrin.net",
-	url = "http://only.mawhrin.net/~mss/thingies/ljpy/",
-	packages = ['livejournal'],
-        scripts = [ 'ljp.py', 'ljac.py' ],
-        license = 'GPL v2'
+    author = "Mikhail Sobolev",
+    author_email = "mss@mawhrin.net",
+    url = "http://only.mawhrin.net/~mss/thingies/ljpy/",
+    packages = ['livejournal'],
+    scripts = [ 'ljp.py', 'ljac.py' ],
+    license = 'GPL v2'
 )
